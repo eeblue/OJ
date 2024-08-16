6fbdac7cceda7eb18d4c43cc6483c51222656acd
eeblueee <138195364+eeblueee@users.noreply.github.com>
2024-08-16 14:27:25 +0800

    Update MatrixDiagonalSum/m_d_sum1.c

--- MatrixDiagonalSum/m_d_sum1.c
+++ MatrixDiagonalSum/m_d_sum1.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int n, sum, i, j = 0;
+    int i, j, n, sum = 0;
     scanf("%d", &n);
-    int a[n][n];
+    int m[n][n];
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++)
-            scanf("%d", &a[i][j]);
+            scanf("%d", &m[i][j]);
     for (i = 0; i < n; i++)
+    {
         for (j = 0; j < n; j++)
+        {
             if (i == j || i + j == n - 1)
-                sum += a[i][j];
+                sum += m[i][j];
+        }
+    }
     printf("%d", sum);
     return 0;
 }
